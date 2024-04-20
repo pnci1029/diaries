eb24153dc8e1e6269b5640c1c6b6126c607045a7
chhong <81909140+pnci1029@users.noreply.github.com>
2024-04-20 16:28:24 +0900

    24.04.20 set test directory

---
+++ diary_sample/src/test/java/com/example/diary_sample/feature/article/domain/ArticleRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.example.diary_sample.feature.article.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleRepositoryTest {
+    @DisplayName("검색 조건에 충족되는 게시물들을 조회한다.")
+    @Test
+    void searchArticles() {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
