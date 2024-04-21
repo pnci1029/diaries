9b995874c4eebb703378f777936340f77f3162b4
chhong <81909140+pnci1029@users.noreply.github.com>
2024-04-21 13:23:50 +0900

    24.04.21 add respository searching test2

--- diary_sample/src/test/java/com/example/diary_sample/feature/article/domain/ArticleRepositoryTest.java
+++ diary_sample/src/test/java/com/example/diary_sample/feature/article/domain/ArticleRepositoryTest.java
@@ -1,9 +1,8 @@
 package com.example.diary_sample.feature.article.domain;
 
-import com.example.diary_sample.feature.article.dto.ArticleCreateRequest;
 import com.example.diary_sample.feature.article.dto.ArticleSearchDto;
 import com.example.diary_sample.feature.article.dto.ArticleServiceCreateRequest;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +20,14 @@ class ArticleRepositoryTest {
     @Autowired
     ArticleRepository articleRepository;
 
-    @DisplayName("검색 조건에 충족되는 게시물들을 조회한다.")
+    @AfterEach
+    void tearDown() {
+        articleRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("검색 조건을 전체로 했을 때 모두 조회한다.")
     @Test
-    void searchArticles() {
+    void searchAllArticles() {
         // given
         String title1 = "title1";
         String title2 = "title2";
@@ -35,11 +39,44 @@ class ArticleRepositoryTest {
 
         articleRepository.saveAll(List.of(Article.create(article1), Article.create(article2)));
 
+        ArticleSearchDto search = ArticleSearchDto.builder()
+                .build();
+
         // when
+        List<Article> result = articleRepository.searchArticles(search);
+
+        // then
+        assertThat(result).hasSize(2);
+        assertThat(result).isNotEmpty();
+        assertThat(result)
+                .extracting("title", "content")
+                .containsExactlyInAnyOrder(
+                        tuple(title1, content1),
+                        tuple(title2, content2)
+                );
+
+    }
+
+    @DisplayName("제목과 내용 검색 정보에 충족되는 게시물들을 조회한다.")
+    @Test
+    void searchArticlesByTitleAndContent() {
+        // given
+        String title1 = "title1";
+        String title2 = "title2";
+        String content1 = "content1";
+        String content2 = "content2";
+
+        ArticleServiceCreateRequest article1 = createArticles(title1, content1);
+        ArticleServiceCreateRequest article2 = createArticles(title2, content2);
+
+        articleRepository.saveAll(List.of(Article.create(article1), Article.create(article2)));
+
         ArticleSearchDto search = ArticleSearchDto.builder()
                 .title(title1)
                 .content(content1)
                 .build();
+
+        // when
         List<Article> result = articleRepository.searchArticles(search);
 
         // then
@@ -48,9 +85,11 @@ class ArticleRepositoryTest {
         assertThat(result)
                 .extracting("title", "content")
                 .containsExactlyInAnyOrder(
-                        tuple("title1", "content1")
+                        tuple(title1, content1)
                 );
     }
+
+
 
     private ArticleServiceCreateRequest createArticles(String title, String content) {
         return ArticleServiceCreateRequest.builder()
