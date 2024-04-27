14583aad9dd52cd985cfd3d6ee10a87babdc886e
chhong <81909140+pnci1029@users.noreply.github.com>
2024-04-27 13:14:53 +0900

    24.04.27 set up for article test

--- diary/src/main/kotlin/sample/diary/dto/article/ArticleServiceRequest.kt
+++ diary/src/main/kotlin/sample/diary/dto/article/ArticleServiceRequest.kt
@@ -4,9 +4,14 @@ import lombok.Builder
 import lombok.Getter
 
 @Getter @Builder
-class ArticleServiceRequest (
-    val title: String,
-    val content: String,
-        ){
+class ArticleServiceRequest(
+    val title: String = "",
+    val content: String = "",
+
+){
+    data class ArticleServiceRequest(
+        var title: String = "",
+        var content: String = ""
+    )
 
 }
