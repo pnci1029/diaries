4ce0e1d9711d4d88669b561c1e153b1cc455dc20
chhong <81909140+pnci1029@users.noreply.github.com>
2024-04-28 16:07:01 +0900

    24.04.28 fix controller test

--- diary_sample/src/test/java/com/example/diary_sample/feature/article/api/ArticleControllerTest.java
+++ diary_sample/src/test/java/com/example/diary_sample/feature/article/api/ArticleControllerTest.java
@@ -2,38 +2,21 @@ package com.example.diary_sample.feature.article.api;
 
 import com.example.diary_sample.feature.article.dto.ArticleCreateRequest;
 import com.example.diary_sample.feature.article.support.ControllerTestSupport;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = {ArticleController.class,
-})
-class ArticleControllerTest  {
-
-    @Autowired
-    protected MockMvc mockMvc;
-
-    @Autowired
-    protected ObjectMapper om;
-
-    @MockBean
-    protected ArticleService articleService;
+class ArticleControllerTest extends ControllerTestSupport {
 
     @DisplayName("게시글을 생성하는 요청을 받아 처리한다.")
     @Test
@@ -41,13 +24,13 @@ class ArticleControllerTest  {
         // given
         LocalDateTime now = LocalDateTime.now();
         ArticleCreateRequest request = ArticleCreateRequest.builder()
-                .title("")
-                .content("")
-                .images(new ArrayList<>())
+                .title("title")
+                .content("content")
+                .images(List.of("1","2"))
                 .createdAt(now)
                 .build();
 
-        // when
+        // when // then
         mockMvc.perform(MockMvcRequestBuilders.post("/api/article/create")
                         .content(om.writeValueAsString(request))
                         .contentType(MediaType.APPLICATION_JSON)
@@ -58,8 +41,32 @@ class ArticleControllerTest  {
                 .andExpect(jsonPath("$.statusNumber").value("200"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data").isEmpty())
         ;
+    }
 
-        // then
+    @DisplayName("게시글 생성 요청 시 이미지가 포함되어있지 않으면 예외처리를 한다.")
+    @Test
+    void createArticleWithoutImage() throws Exception {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+        ArticleCreateRequest request = ArticleCreateRequest.builder()
+                .title("title")
+                .content("content")
+                .images(null)
+                .createdAt(now)
+                .build();
+
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/article/create")
+                        .content(om.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.statusNumber").value(400))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("이미지 속성은 필수입니다."))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data").isEmpty())
+        ;
     }
 
     private ArticleCreateRequest createArticleSupport(
