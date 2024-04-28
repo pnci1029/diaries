a2e40da44fbb7509eda61c9dcbfd7bc48d4ea83d
chhong <81909140+pnci1029@users.noreply.github.com>
2024-04-28 13:52:47 +0900

    24.04.28 setup images

--- diary_sample/src/main/java/com/example/diary_sample/feature/image/domain/Image.java
+++ diary_sample/src/main/java/com/example/diary_sample/feature/image/domain/Image.java
@@ -3,12 +3,14 @@ package com.example.diary_sample.feature.image.domain;
 import com.example.diary_sample.feature.article.domain.Article;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Image {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long imageId;
     private String imgName;
@@ -16,4 +18,24 @@ public class Image {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "articleId")
     private Article article;
+
+
+    @Builder
+    private Image(String imgName, String imgPath, Article article) {
+        this.imgName = imgName;
+        this.imgPath = imgPath;
+        this.article = article;
+    }
+
+
+    public static Image create(String image, Article article) {
+        if (image.isEmpty()) {
+            throw new IllegalArgumentException("이미지를 추가해주세요");
+        }
+        return Image.builder()
+                .article(article)
+                .imgPath(image)
+                .imgName(image)
+                .build();
+    }
 }
---
+++ diary_sample/src/main/java/com/example/diary_sample/feature/image/domain/ImageRepository.java
@@ -0,0 +1,6 @@
+package com.example.diary_sample.feature.image.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
