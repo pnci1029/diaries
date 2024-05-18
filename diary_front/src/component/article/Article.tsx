98444faaf3993f76f3e75e2943b730b0e88acfe0
chhong <81909140+pnci1029@users.noreply.github.com>
2024-05-18 14:15:53 +0900

    24.05.08 [fe] 라우팅처리2

--- diary_front/src/component/article/Article.tsx
+++ diary_front/src/component/article/Article.tsx
@@ -1,17 +1,24 @@
 import {useArticleGetter} from "./hooks/useArticleGetter";
 import {useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function Article() {
     const { getArticles, articles } = useArticleGetter();
+    const navigator = useNavigate();
 
     useEffect(() => {
         getArticles();
     }, [getArticles]);
 
+    function handleCreator() {
+        navigator(`/article/create`);
+    }
+
     console.log(articles);
     return (
         <>
             <p>article123</p>
+            <button onClick={handleCreator}>post article</button>
         </>
     );
 };
---
+++ diary_front/src/component/article/ArticleCreator.tsx
@@ -0,0 +1,10 @@
+export function ArticleCreator() {
+
+    console.log('creator')
+
+    return (
+        <>
+            <p>creator</p>
+        </>
+    )
+}
