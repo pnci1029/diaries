e22fce9a4cf2545b4d59e546681b82386757a2e0
chhong <pnci1029@gmail.com>
2024-06-06 13:22:27 +0900

    24.06.06 finally fix

--- diary/build.gradle.kts
+++ diary/build.gradle.kts
@@ -1,27 +1,25 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
+    java
     id("org.springframework.boot") version "3.1.1"
-    id("io.spring.dependency-management") version "1.1.4"
-    id("org.jetbrains.kotlin.jvm") version "1.8.21"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.8.21"
-    id("org.jetbrains.kotlin.plugin.noarg") version "1.8.21"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.8.21"
-    id("org.jetbrains.kotlin.kapt") version "1.8.21"
-    id("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
-    id("idea")
+    id("io.spring.dependency-management") version "1.1.5"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+    kotlin("plugin.jpa") version "1.8.21"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
+    kotlin("kapt") version "1.8.21"
 }
 
-group = "sample"
+group = "com.example"
 version = "0.0.1-SNAPSHOT"
-
+val queryDslVersion = "5.0.0"
+val querydslDir = "src/main/generated"
 java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
     compileOnly {
-        extendsFrom(annotationProcessor)
+        extendsFrom(configurations.annotationProcessor.get())
     }
 }
 
@@ -30,36 +28,53 @@ repositories {
 }
 
 dependencies {
-    // dependencies...
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // QueryDsl
+    kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
+    kapt("com.querydsl:querydsl-jpa")
+    implementation("com.querydsl:querydsl-jpa:5.0.0")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+//    implementation("com.querydsl:querydsl-jpa:$queryDslVersion:jakarta")
+//    kapt("jakarta.annotation:jakarta.annotation-api")
+//    kapt("jakarta.persistence:jakarta.persistence-api")
+
+    // Swagger
+    implementation("io.springfox:springfox-swagger-ui:3.0.0")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
+
+    implementation("org.hibernate:hibernate-core:5.6.4.Final")
 }
 
-kapt {
-    correctErrorTypes = true
-    useBuildCache = true
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 sourceSets {
-    main {
-        java {
-            srcDirs("src/main/java", "src/main/generated")
-        }
-        kotlin {
-            srcDirs("src/main/kotlin", "src/main/generated")
-        }
-    }
+    getByName("main").java.srcDirs(querydslDir)
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
-    }
+tasks.withType<JavaCompile> {
+    options.generatedSourceOutputDirectory = file(querydslDir)
 }
 
-idea {
-    module {
-        val kaptMain = file("build/generated/source/kapt/main")
-        sourceDirs.add(kaptMain)
-        generatedSourceDirs.add(kaptMain)
+tasks.named("clean") {
+    doLast {
+        file(querydslDir).deleteRecursively()
     }
 }
