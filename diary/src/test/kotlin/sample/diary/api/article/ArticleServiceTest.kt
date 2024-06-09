a2f4d233162404af1285143189ffeb401570ce3b
chhong <pnci1029@gmail.com>
2024-06-09 15:36:21 +0900

    24.06.09 add & fix test

--- diary/src/test/kotlin/sample/diary/api/article/ArticleServiceTest.kt
+++ diary/src/test/kotlin/sample/diary/api/article/ArticleServiceTest.kt
@@ -1,11 +1,10 @@
 package sample.diary.api.article
 
-import org.assertj.core.api.AssertionsForClassTypes.assertThat
 import org.assertj.core.api.AssertionsForClassTypes.tuple
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,30 +13,31 @@ import org.springframework.http.HttpStatus
 import sample.diary.domain.article.Article
 import sample.diary.domain.article.ArticleRepository
 import sample.diary.dto.article.ArticleServiceRequest
-import sample.diary.mock.MockRepository
-import sample.diary.mock.MockRepository.getArticleRepository
-import sample.diary.mock.MockService
-import sample.diary.mock.MockService.getArticleService
+import org.assertj.core.api.Assertions.assertThat;
 
 // given
 // when
 // then
-//@ExtendWith(MockitoExtension::class)
+@ExtendWith(MockitoExtension::class)
 @SpringBootTest
-internal class ArticleServiceTest{
+class ArticleServiceTest{
 //    private val articleRepository= getArticleRepository()
 //    private val articleService= getArticleService(
 //        articleRepository = articleRepository
 //    )
-//    @BeforeEach
-//    fun beforeEach() {
-//        MockitoAnnotations.initMocks(this)
-//    }
+    @BeforeEach
+    fun beforeEach() {
+        MockitoAnnotations.initMocks(this)
+    }
 
     @Autowired
     private lateinit var articleService: ArticleService
 
+    @Autowired
+    private lateinit var articleRepository: ArticleRepository
+
     @Test
+    @DisplayName("요청 정보를 받아서 게시물을 생성한다.")
     fun createArticle() {
         // given
         val title1 = "title1"
@@ -50,13 +50,49 @@ internal class ArticleServiceTest{
 
         // when
         val result = articleService.createArticle(request1)
-        println(result.body.articleId)
 
         // then
         assertThat(result.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(result.body).extracting("title","content")
             .contains(
                 title1, content1
+            )
+    }
+
+    @Test
+    @DisplayName("전체 게시물을 조회한다.")
+    fun getAllArticles() {
+        //given
+        val title1 = "title1"
+        val content1 = "content1"
+        val title2 = "title2"
+        val content2 = "content2"
+        val title3 = "title3"
+        val content3 = "content3"
+
+        val request1 = createArticles(title1, content1)
+        val request2 = createArticles(title2, content2)
+        val request3 = createArticles(title3, content3)
+
+        val articleList = listOf(
+            Article.create(request1), Article.create(request2), Article.create(request3)
+        )
+
+        articleRepository.saveAll(articleList)
+
+        //when
+        val result = articleService.getArticles()
+
+        //then
+        assertThat(result.size).isEqualTo(articleList.size)
+        assertThat(result).isInstanceOf(MutableList::class.java)
+
+        assertThat(result)
+            .extracting("title", "content")
+            .containsExactlyInAnyOrder(
+                tuple(title1, content1),
+                tuple(title2, content2),
+                tuple(title3, content3)
             )
     }
 
